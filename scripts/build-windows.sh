037b571c9bebb261b706fc6ff04e921cedb2aa01
Alexander O'Mara <me@alexomara.com>
2020-03-21 15:31:18 -0400

    Windows set generation types

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -6,29 +6,29 @@ set -o pipefail
 rm -rf build-windows-win64-debug
 mkdir build-windows-win64-debug
 pushd build-windows-win64-debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Debug ..
+cmake --config Debug --build .
 popd
 
 rm -rf build-windows-win64-release
 mkdir build-windows-win64-release
 pushd build-windows-win64-release
-cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Release ..
+cmake --config Release --build .
 popd
 
 rm -rf build-windows-win32-debug
 mkdir build-windows-win32-debug
 pushd build-windows-win32-debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
+cmake -DCMAKE_GENERATOR_PLATFORM=x86 -DCMAKE_BUILD_TYPE=Debug ..
+cmake --config Debug --build .
 popd
 
 rm -rf build-windows-win32-release
 mkdir build-windows-win32-release
 pushd build-windows-win32-release
-cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
+cmake -DCMAKE_GENERATOR_PLATFORM=x86 -DCMAKE_BUILD_TYPE=Release ..
+cmake --config Release --build .
 popd
 
 mkdir -p dist
