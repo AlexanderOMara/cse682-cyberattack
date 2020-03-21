a495374750f9eceeed1bc1dd1700997f23c57ff2
Alexander O'Mara <me@alexomara.com>
2020-03-21 16:10:49 -0400

    Trying to fix Windows build config

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -7,16 +7,16 @@ rm -rf build-windows-win64
 mkdir build-windows-win64
 pushd build-windows-win64
 cmake -A x64 ..
-cmake --config Debug --build .
-cmake --config Release --build .
+cmake --build . --config Debug
+cmake --build . --config Release
 popd
 
 rm -rf build-windows-win32
 mkdir build-windows-win32
 pushd build-windows-win32
 cmake -A Win32 ..
-cmake --config Debug --build .
-cmake --config Release --build .
+cmake --build . --config Debug
+cmake --build . --config Release
 popd
 
 mkdir -p dist
