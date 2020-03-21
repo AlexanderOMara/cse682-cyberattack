9587ac06782b2bc35af3fc70fdc6969ba990e019
Alexander O'Mara <me@alexomara.com>
2020-03-21 15:47:52 -0400

    Fixing Windows build configuration

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -3,34 +3,24 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-rm -rf build-windows-win64-debug
-mkdir build-windows-win64-debug
-pushd build-windows-win64-debug
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Debug ..
+rm -rf build-windows-win64
+mkdir build-windows-win64
+pushd build-windows-win64
+cmake -G "Visual Studio 17 Win64" ..
 cmake --config Debug --build .
-popd
-
-rm -rf build-windows-win64-release
-mkdir build-windows-win64-release
-pushd build-windows-win64-release
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Release ..
 cmake --config Release --build .
 popd
 
-rm -rf build-windows-win32-debug
-mkdir build-windows-win32-debug
-pushd build-windows-win32-debug
-cmake -DCMAKE_GENERATOR_PLATFORM=x86 -DCMAKE_BUILD_TYPE=Debug ..
+rm -rf build-windows-win32
+mkdir build-windows-win32
+pushd build-windows-win32
+cmake -G "Visual Studio 17" ..
 cmake --config Debug --build .
-popd
-
-rm -rf build-windows-win32-release
-mkdir build-windows-win32-release
-pushd build-windows-win32-release
-cmake -DCMAKE_GENERATOR_PLATFORM=x86 -DCMAKE_BUILD_TYPE=Release ..
 cmake --config Release --build .
 popd
 
 mkdir -p dist
-cp build-windows-win64-release/bin/CyberAttack.exe dist/CyberAttack-windows-win64.exe
-cp build-windows-win32-release/bin/CyberAttack.exe dist/CyberAttack-windows-win32.exe
+find build-windows-win64
+find build-windows-win32
+# cp build-windows-win64-release/bin/CyberAttack.exe dist/CyberAttack-windows-win64.exe
+# cp build-windows-win32-release/bin/CyberAttack.exe dist/CyberAttack-windows-win32.exe
